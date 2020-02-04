5b1b80a7a399ac1ba2561683c58d8aca8d79700e
Fiorella <fsobenes@gmail.com>
2020-02-03 19:37:01 -0700

    forgot about linking js

--- js/commitment.js
+++ js/commitment.js
@@ -1,8 +1,18 @@
-window.onscroll = function() {scrollFunction()};
+var nav = document.getElementById("navbar");
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
+window.scroll = function() {
+	if (window.pageYOffset > 100){
+		nav.style.opacity = "0.9"
+	}
+}
+
+
+
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-50px";
+//   }
