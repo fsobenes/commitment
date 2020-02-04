e627f8a1c4a8ad8c301be2b8ead674b7fe80e1de
Fiorella <fsobenes@gmail.com>
2020-02-03 19:31:23 -0700

    tried js code

--- js/commitment.js
+++ js/commitment.js
@@ -1,1 +1,8 @@
-commitment.js
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
