41c18551e103aa755245a6569ad9b64b5816d9ff
Fiorella <fsobenes@gmail.com>
2020-02-03 20:03:56 -0700

    WHOOO try 4

--- js/commitment.js
+++ js/commitment.js
@@ -3,13 +3,13 @@ var nav = document.getElementById("navbar");
 window.onscroll = function(){
 	if (window.pageYOffset <500){
 		nav.style.opacity = "0";
-	else if (window.pageYOffset >500{
+	}
+	else{ //if (window.pageYOffset >=500{
 		nav.style.opacity = "0.8";
 	}
 
 		//nav.style.opacity = "0.9";
 		//nav.style.boxShadow = "0px 2px 1px pink";
-	}
 	
 }
 
